2f4b98a7071b1259f518de9bbfeeef6962542c07
Yash Parmar <0808io201056.ies@ipsacademy.org>
2025-11-03 19:24:17 +0530

    Update footer styles in layout component for improved aesthetics and consistency.

--- app/layout.jsx
+++ app/layout.jsx
@@ -21,8 +21,8 @@ export default function RootLayout({ children }) {
         <main className="min-h-screen">{children}</main>
         <Toaster position="bottom-right" richColors />
         <footer>
-          <div className="bg-blue-500 text-white font-bold py-12 bg-opacity-10">
-            <p className="mx-auto px-4 text-center text-gray-900">Made with 💗 by Yash Parmar</p>
+          <div className="bg-white font-bold py-12 bg-opacity-10">
+            <p className="mx-auto px-4 font-bold text-center text-gray-900">Made with 💗 by Yash Parmar</p>
           </div>
         </footer>
       </body>
